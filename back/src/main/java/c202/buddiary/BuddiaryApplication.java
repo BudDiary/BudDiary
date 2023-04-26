c4cd953defea039c494c0fc6ed1028cd49e47788
ManduTheCat <krocd@naver.com>
2023-04-26 10:25:57 +0900

    init

---
+++ back/src/main/java/c202/buddiary/BuddiaryApplication.java
@@ -0,0 +1,13 @@
+package c202.buddiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddiaryApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/c202/buddiary/BuddiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package c202.buddiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
