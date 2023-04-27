5bd34f6b0227613af75db85138b8b2622122fe32
ManduTheCat <krocd@naver.com>
2023-04-27 09:45:04 +0900

    feature:  check 스타일 적용

--- back/src/main/java/twozerotwo/buddiary/global/aop/LogAop.java
+++ back/src/main/java/twozerotwo/buddiary/global/aop/LogAop.java
@@ -2,7 +2,6 @@ package twozerotwo.buddiary.global.aop;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -24,14 +23,4 @@ public class LogAop {
 		log.info("[log] {} -> {} 종료", joinPoint.getSignature().getDeclaringType().getSimpleName(),
 			joinPoint.getSignature().getName());
 	}
-
-	// @AfterThrowing(value =
-	// 	"execution(* twozerotwo.buddiary..*(..)) && !execution(* com.c211.opinbackend.config..*(..))"
-	// 		+ "&& !execution(* twozerotwo.buddiary.global.auth.*(..))", throwing = "exception")
-	// public void writeFailLog(JoinPoint joinPoint, Exception exception) throws RuntimeException {
-	// 	//logging
-	// 	//exception 으로 해당 메서드에서 발생한 예외가져오기 가능
-	// 	exception.printStackTrace();
-	// }
-
 }
