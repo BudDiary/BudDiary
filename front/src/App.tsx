05637967b6635b3e1a55122e01690caacfe86203
ats166 <ats166@naver.com>
2023-04-27 12:02:56 +0900

    Style: 페이지 라우터 설정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,28 +1,36 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import useMember from './hooks/memberHook';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import useMember from "./hooks/memberHook";
+import "./App.css";
 
-import Home from './pages/home/home.component';
-// import NotFound from '@/pages/NotFound';
+import NotFound from "./pages/notfound/NotFoundPage";
+import WritePage from "./pages/write/WritePage";
+import GroupDetailPage from "./pages/groupdetail/GroupDetailPage";
+import GroupPage from "./pages/group/GroupPage";
+import SurveyPage from "./pages/survey/SurveyPage";
+import HomePage from "./pages/home/HomePage";
+import MypagePage from "./pages/mypage/MypagePage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-    // errorElement: <NotFound />,
+    errorElement: <NotFound />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/survey", element: <SurveyPage /> },
+      { path: "/group", element: <GroupPage /> },
+      { path: "/group/:id", element: <GroupDetailPage /> },
+      { path: "/write", element: <WritePage /> },
+      { path: "/mypage", element: <MypagePage /> },
+    ],
   },
 ]);
 
-
 export default function App() {
-  const { isLoggedIn } = useMember(); 
+  const { isLoggedIn } = useMember();
   return (
     <div className="App">
-      <div className={isLoggedIn ? 'page' : 'login'}>
+      <div className={isLoggedIn ? "page" : "login"}>
         <RouterProvider router={router} />
       </div>
     </div>
   );
 }
-
-
