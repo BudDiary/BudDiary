ab9c0f5290eb494779a54d0c83feee4a58b0c5b3
민우 <meanwo0603@naver.com>
2023-04-28 11:12:40 +0900

    Feat: 선택 이미지 출력 수정중

---
+++ front/src/components/write/Pictures.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
+
+interface Props {
+  originFiles: File[];
+}
+
+export default function Pictures({ originFiles }: Props) {
+  return (
+    <Swiper
+    pagination={{
+      type: 'fraction',
+    }}
+    navigation={true}
+    modules={[Pagination, Navigation]}
+    
+  >
+    {originFiles.map((file, index) => (
+      <SwiperSlide key={index}>
+        <img src={URL.createObjectURL(file)} alt={'사진'} />
+      </SwiperSlide>
+    ))}
+  </Swiper>
+  );
+}
