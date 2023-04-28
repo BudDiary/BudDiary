5e6e1800d23a1b153fea8ed23544b84f7f121d32
ManduTheCat <krocd@naver.com>
2023-04-28 17:58:45 +0900

    feature: userDetailService 분리 설계

--- back/src/main/java/twozerotwo/buddiary/global/auth/service/LoginService.java
+++ back/src/main/java/twozerotwo/buddiary/global/auth/service/LoginService.java
@@ -3,8 +3,16 @@ package twozerotwo.buddiary.global.auth.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import twozerotwo.buddiary.persistence.repository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
 public class LoginService implements UserDetailsService {
+	private final MemberRepository memberRepository;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		return null;
