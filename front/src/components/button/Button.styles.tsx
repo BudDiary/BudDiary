3a31c0ebbea793033daea80475b5664d433f129a
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-04-28 11:12:06 +0900

    Feat: 마이페이지 모달

--- front/src/components/button/Button.styles.tsx
+++ front/src/components/button/Button.styles.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 
-export const FullRoundedButton = styled.div`
+export const FullRoundedButton = styled.button`
     ${tw`bg-bud-blue`}
 `
