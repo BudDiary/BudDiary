ba16d94131a91d92bed70b03fbf320a29174ec52
민우 <meanwo0603@naver.com>
2023-04-28 10:33:08 +0900

    Feat: 이미지 업로드 기능 추가

--- front/src/pages/write/WritePage.tsx
+++ front/src/pages/write/WritePage.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useState } from 'react';
+import Addpicture from "../../components/write/Addpicture";
+
+import { PostingTabType } from "../../types/write";
 
 export default function WritePage() {
-  return <div>WritePage!!</div>;
+  const [tab, setTab] = useState<PostingTabType>('SELECT_IMAGE');
+  const [originFiles, setOriginFiles] = useState<File[]>([]);
+  return <div>
+    {tab === 'SELECT_IMAGE' && (
+      <Addpicture setTab={setTab} originFiles={originFiles} setOriginFiles={setOriginFiles} />
+    )}
+  </div>;
 }
