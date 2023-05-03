ea9e01e3479bbe50505dd9c5db6a4ef1ae734612
민우 <meanwo0603@naver.com>
2023-05-03 10:04:23 +0900

    Feat: 일기 작성 swipe 및 사진 삭제 버튼 수정

--- front/src/components/write/Pictures.tsx
+++ front/src/components/write/Pictures.tsx
@@ -1,32 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper";
+import { GiCancel } from "react-icons/gi"
 
 interface Props {
   originFiles: File[];
+  setOriginFiles: React.Dispatch<React.SetStateAction<File[]>>;
 }
 
+export default function Pictures({ originFiles, setOriginFiles  }: Props) {
+  const [photoList, setPhotoList] = useState(originFiles);
+  useEffect(() => {
+    console.log(photoList);
+  }, [photoList]);
+  const handleDelete = (index: number) => {
+    const newList = [...photoList];
+    newList.splice(index, 1);
+    setPhotoList(newList);
+    setOriginFiles(newList);
+  };
 
-export default function Pictures({ originFiles }: Props) {
+  useEffect(() => {
+    setPhotoList(originFiles);
+  }, [originFiles]);
+
   return (
     <>
-    <Swiper
-    pagination={{
-      type: 'fraction',
-    }}
-    navigation={true}
-    modules={[Pagination, Navigation]}
-    
-    >
-    {originFiles.map((file, index) => (
-      <SwiperSlide key={index}>
-        <img src={URL.createObjectURL(file)} alt={'사진'} />
-      </SwiperSlide>
-    ))}
-  </Swiper>
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Navigation]}
+      >
+        {photoList.map((file, index) => (
+          <SwiperSlide
+            key={index}
+            style={{ display: "flex", justifyContent: "center", alignItems: "center" , minWidth: "250px" }}
+          >
+            <img
+              src={URL.createObjectURL(file)}
+              alt={"사진"}
+              style={{ maxWidth: "100%", maxHeight: "100%" }}
+            />
+            <div>
+              <GiCancel color="red" min={500} onClick={() => handleDelete(index)}></GiCancel>
+            </div>
+          </SwiperSlide>
+          
+        ))}
+      </Swiper>
     </>
   );
 }
