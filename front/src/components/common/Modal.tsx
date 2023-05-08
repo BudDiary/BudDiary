36ee9884ff98957e7f56d08bb8c8a3ddaeb3dd14
민우 <meanwo0603@naver.com>
2023-05-08 10:45:13 +0900

    Feat: 새 그룹일기 생성 모달

---
+++ front/src/components/common/Modal.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useEffect, useRef, useState } from 'react';
+import NewGroupDiaryModal from '../group/NewGroupDiaryModal';
+import ProfileEditModal from '../mypage/ProfileEditModal';
+interface Props {
+    page: number;
+    setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  }
+export default function Modal({ page, setModalOpen }: Props) {
+	const closeModal = () => {
+		setModalOpen(false);
+    }
+  return (
+    <>
+    {(page === 1) && (<NewGroupDiaryModal closeModal={closeModal}/>)}
+    {(page === 2) && (<ProfileEditModal/>)}
+    </>
+    
+
+  )
+
+}
--- front/src/components/group/NewGroupDiaryModal.tsx
+++ front/src/components/group/NewGroupDiaryModal.tsx
@@ -1,29 +1,115 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import Button from '@mui/joy/Button';
 import {BiArrowBack} from 'react-icons/bi'
-import { ModalContainer, BackgroundContainer, ModalTopNavContainer, CloseModalButton, ModalTitle, SaveModalButton } from '../common/Modal.styles'
-import { SurveyAgainButton } from "../common/Button.styles";
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+import Typography from '@mui/joy/Typography';
+import Sheet from '@mui/joy/Sheet';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import { Box } from '@mui/material';
+import Paper from '@mui/material/Paper';
+
+import { GiCancel } from "react-icons/gi"
+import { CloseModalButton, ModalTitle, ModalTopNavContainer, SaveModalButton } from '../common/Modal.styles';
+import AddGroupPicture from './AddGroupPicture';
+import GroupPicture from './GroupPicture';
+
 
 interface Props {
-    setNewDiaryModal: React.Dispatch<React.SetStateAction<boolean>>;
-  }
+  closeModal: any;
+}
 
-  export default function NewGroupDiaryModal({ setNewDiaryModal }: Props) {
-    const closeDiaryModal = () => {
-        setNewDiaryModal(false);  }
+
+export default function NewGroupDiaryModal({ closeModal }: Props) {
+  const [originFiles, setOriginFiles] = useState<File[]>([]);
+
+  const closeDiaryModal = () => {
+    closeModal();
+  };
+  const [value, setValue] = useState('');
+
+  const handleClear = () => {
+    setValue('');
+  }
+  
   return (
-    <>
-      <BackgroundContainer>gg</BackgroundContainer>
-      <ModalContainer>
-        <ModalTopNavContainer>
-          <CloseModalButton><BiArrowBack /></CloseModalButton>
-          <ModalTitle>프로필 수정</ModalTitle>
-          <SaveModalButton  onClick={closeDiaryModal}>완료</SaveModalButton>
-        </ModalTopNavContainer>
-        {/* <div className="w-[200px] h-[200px]"></div> */}
-        <input type="file"></input>
-        <input type="text" placeholder="닉네임"></input>
-        <input type="text" placeholder="소개글"></input>
-      </ModalContainer>
-    </>
-  )
+    <React.Fragment>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={true}
+        onClose={() => closeDiaryModal()}
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{
+            minHeight: 300,
+            minWidth: 500,
+            maxWidth: 500,
+            borderRadius: 'md',
+            // p: 5,
+            boxShadow: 'lg',
+          }}
+          >
+          <ModalTopNavContainer>
+          <CloseModalButton onClick={closeDiaryModal}><BiArrowBack /></CloseModalButton>
+          <ModalTitle>새 교환일기</ModalTitle>
+
+          <ModalClose
+            variant="outlined"
+            sx={{
+              top: 'calc(-1/4 * var(--IconButton-size))',
+              right: 'calc(-1/4 * var(--IconButton-size))',
+              boxShadow: '0 2px 12px 0 rgba(0 0 0 / 0.2)',
+              borderRadius: '50%',
+              bgcolor: 'background.body',
+            }}
+          />
+          <SaveModalButton>완료</SaveModalButton>
+          </ModalTopNavContainer>
+          <Typography
+            component="h2"
+            id="modal-title"
+            level="h4"
+            textColor="inherit"
+            fontWeight="lg"
+            mt={2}
+            ml={3}
+            mb={2}
+          >
+            방 이름
+          </Typography>
+          <Box marginLeft={3}>
+            
+          <TextField
+      value={value}
+      onChange={(e:any) => setValue(e.target.value)}
+      InputProps={{
+        endAdornment: value && (
+          <IconButton onClick={handleClear}>
+            <GiCancel color="lightcoral" size={20}/>
+          </IconButton>
+        ),
+      }}
+      label="그룹일기"
+      variant="outlined"
+      />
+      </Box>
+      <Box m={2}>
+      <Paper elevation={3}>
+        <AddGroupPicture
+          originFiles={originFiles}
+          setOriginFiles={setOriginFiles}/>
+        <GroupPicture
+          originFiles={originFiles}
+          setOriginFiles={setOriginFiles}/>
+      </Paper>
+
+    </Box>
+        </Sheet>
+      </Modal>
+    </React.Fragment>
+  );
 }
