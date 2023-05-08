c0991517b1361e44d85630cfd775b24074d52061
ManduTheCat <krocd@naver.com>
2023-05-08 16:03:19 +0900

    fix : 리뷰 내용 반영

--- back/src/main/java/twozerotwo/buddiary/domain/member/dto/MemberSignUpRequest.java
+++ back/src/main/java/twozerotwo/buddiary/domain/member/dto/MemberSignUpRequest.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @NoArgsConstructor
 public class MemberSignUpRequest {
-	@Size(max = 8, min = 2, message = "2자 이상 8 이하로 username 을 작성하세요")
+	@Size(max = 8, min = 2, message = "2자 이상 8 이하로 nickname 을 작성하세요")
 	@NotBlank(message = "nickname 이 비어 있습니다.")
 	private String nickname;
 
