9b3e3ea011207ce654803b4bd9c8df9de9c43c65
ManduTheCat <krocd@naver.com>
2023-05-10 15:49:55 +0900

    feature :controller 작성

--- back/src/main/java/twozerotwo/buddiary/domain/club/api/ClubController.java
+++ back/src/main/java/twozerotwo/buddiary/domain/club/api/ClubController.java
@@ -69,20 +69,11 @@ public class ClubController {
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
 
-	//------------------------------------------클럽 초대 링크 생성----------------------------------------------
-	@GetMapping("/invitation/{clubId}")
-	public ResponseEntity inviteClub(@PathVariable("clubId") String clubId) {
-		// /invitation/{clubId} 형태로 제공
-		return null;
-	}
-
 	//------------------------------------------클럽 초대 링크 수신----------------------------------------------
-	@PostMapping("/invitation/{clubId}")
+	@PostMapping("/invitation/{clubId}/{username}")
 	public ResponseEntity inviteClub(@PathVariable("clubId") String clubId, @PathVariable String username) {
-		// 로그인 안한친구는 로그인 하고 응해야한다.
-		// 로그인 안한 친구가 온다면
-		// 로그인 한 친구가 온다면
-		
+		// 로그인 거치고 이 api를 받는다
+
 		return null;
 	}
 
