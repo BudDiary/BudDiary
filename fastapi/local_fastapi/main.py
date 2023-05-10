14a145d210889f333ba554731721c4604c6ddebb
민우 <meanwo0603@naver.com>
2023-05-10 15:59:25 +0900

    Feat: 초기 설문조사 fastapi 전송

--- fastapi/local_fastapi/main.py
+++ fastapi/local_fastapi/main.py
@@ -4,8 +4,11 @@
 
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
-
+class myAnswer(BaseModel):
+    id: int
+    favor_list: list[int]
 
 app = FastAPI()
 origins = [
@@ -24,3 +27,14 @@ app.add_middleware(
 @app.get("/fastapi/test")
 def test():
     return {'test': 'complete'}
+
+
+@app.post("/fastapi/survey")
+def survey(items: myAnswer):
+    survey_object = {
+        'id': items.id,
+        'preference': items.favor_list
+    }
+
+
+    return survey_object
