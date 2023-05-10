cdf27ba1784f88115a0dc046ee2ea427f8357bbf
ManduTheCat <krocd@naver.com>
2023-05-10 22:31:08 +0900

    style : 필요없는 임포트 제거

--- back/src/test/java/twozerotwo/buddiary/domain/club/api/ClubControllerTest.java
+++ back/src/test/java/twozerotwo/buddiary/domain/club/api/ClubControllerTest.java
@@ -4,20 +4,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import twozerotwo.buddiary.annotaion.WithAuthUser;
-import twozerotwo.buddiary.domain.member.api.MemberController;
-import twozerotwo.buddiary.domain.member.service.MemberService;
-import twozerotwo.buddiary.persistence.repository.MemberRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
