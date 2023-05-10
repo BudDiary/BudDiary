06e1c067d6a38d001582df0995f56526017a94de
ManduTheCat <krocd@naver.com>
2023-05-10 20:57:03 +0900

    feature : set test

---
+++ back/src/test/java/twozerotwo/buddiary/domain/club/service/serviceTest.java
@@ -0,0 +1,12 @@
+package twozerotwo.buddiary.domain.club.service;
+
+import org.hibernate.sql.ordering.antlr.Factory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+public class serviceTest {
+
+}
