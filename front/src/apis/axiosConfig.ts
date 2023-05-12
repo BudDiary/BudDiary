f6ffa4102610978d8693c2291afdbb97d1cd25bf
ManduTheCat <krocd@naver.com>
2023-05-12 22:18:54 +0900

    feature : alb 안거치게 수정

--- front/src/apis/axiosConfig.ts
+++ front/src/apis/axiosConfig.ts
@@ -4,12 +4,12 @@ import axios from "axios";
 
 // 로그인된 사용자
 // const BASE_URL = "http://localhost:8080";
-const BASE_URL = "http://buddiaryALB-1250245218.ap-northeast-2.elb.amazonaws.com";
-const FASTAPI_BASE_URL = "http://localhost:9000"
+const BASE_URL =
+  "http://buddiaryALB-1250245218.ap-northeast-2.elb.amazonaws.com";
+const FASTAPI_BASE_URL = "http://localhost:9000";
 // const BASE_URL = "http://3.35.197.93:8080";
 // const BASE_URL = "http://172.31.144.1:8080";
 // const BASE_URL = "http://192.168.100.175:8080";
-
 
 axios.defaults.baseURL = BASE_URL;
 
@@ -19,15 +19,14 @@ const api = axios.create({
 
 const authApi = axios.create({
   baseURL: BASE_URL,
-  headers: {
-  }
+  headers: {},
 });
 
 const formApi = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'Content-Type': 'multipart/form-data',
-  }
+    "Content-Type": "multipart/form-data",
+  },
 });
 
 // fastApi
@@ -37,12 +36,13 @@ const fastApi = axios.create({
 
 // 카카오톡 로그인
 // const REDIRECT_URI = "http://localhost:3000/login/oauth2/code/kakao";
-const REDIRECT_URI = "http://ec2-3-36-102-176.ap-northeast-2.compute.amazonaws.com/login/oauth2/code/kakao";
+const REDIRECT_URI =
+  "http://ec2-3-36-102-176.ap-northeast-2.compute.amazonaws.com/login/oauth2/code/kakao";
 // const KAKAO_AUTH_URL = `http://localhost:8080/oauth2/authorize/kakao?redirect_uri=${REDIRECT_URI}`;
-const KAKAO_AUTH_URL = `http://buddiaryALB-1250245218.ap-northeast-2.elb.amazonaws.com/oauth2/authorize/kakao?redirect_uri=${REDIRECT_URI}`;
-console.log(KAKAO_AUTH_URL)
+const KAKAO_AUTH_URL = `http://ec2-3-35-197-93.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorize/kakao?redirect_uri=${REDIRECT_URI}`;
+console.log(KAKAO_AUTH_URL);
 const kakaoApi = axios.create({
   baseURL: KAKAO_AUTH_URL,
 });
 
-export { axios, api, formApi, kakaoApi,fastApi, KAKAO_AUTH_URL, authApi};
+export { axios, api, formApi, kakaoApi, fastApi, KAKAO_AUTH_URL, authApi };
--- front/src/pages/signup/SignUpPage.tsx
+++ front/src/pages/signup/SignUpPage.tsx
@@ -1,39 +1,39 @@
-import React, {useEffect} from 'react'
-import { kakaoSignUpApi } from '../../apis/userApi';
+import React, { useEffect } from "react";
+import { kakaoSignUpApi } from "../../apis/userApi";
 // import { REDIRECT_URI } from '../../apis/axiosConfig';
 import { useNavigate } from "react-router-dom";
-import useMember from '../../hooks/memberHook';
-import Swal from 'sweetalert2';
+import useMember from "../../hooks/memberHook";
+import Swal from "sweetalert2";
 export default function SignUpPage() {
   const navigate = useNavigate();
   const { login } = useMember();
   useEffect(() => {
     async function fetchData() {
       const currentUrl: string = window.location.href;
-      const code = currentUrl.split('ec2-3-36-102-176.ap-northeast-2.compute.amazonaws.com/')[1];
+      const code = currentUrl.split(
+        "ec2-3-36-102-176.ap-northeast-2.compute.amazonaws.com/"
+      )[1];
       // const code = currentUrl.split('localhost:3000/')[1];
-      const response = await kakaoSignUpApi(code)
-      console.log(response)
+      console.log(code);
+      const response = await kakaoSignUpApi(code);
+      console.log(response);
       // 처음 가입한 사람이면 signup 으로 보내고, 아니면 메인페이지로
-      if (response.newBe === true ) {
-        navigate('/signup-info', {state:response})
+      if (response.newBe === true) {
+        navigate("/signup-info", { state: response });
       } else if (response.newBe === false) {
         // 이미 가입된 사용자이면
-        login(response)
+        login(response);
         // if (response.)
         Swal.fire({
-          icon: 'success',
-          text: '로그인 성공!'
-        })
-        navigate('/')
-        
+          icon: "success",
+          text: "로그인 성공!",
+        });
+        navigate("/");
       } else {
-        console.log(response)
+        console.log(response);
       }
     }
     fetchData();
   }, []);
-  return (
-    <div>SignUpPage</div>
-  )
+  return <div>SignUpPage</div>;
 }
