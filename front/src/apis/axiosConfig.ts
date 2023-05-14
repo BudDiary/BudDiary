7f879b85ed95d0955a3dea66fd278ae7be0c3b8a
ManduTheCat <krocd@naver.com>
2023-05-14 18:29:10 +0900

    feature : add front env and delete cookie authorizationRequestRepository

--- front/src/apis/axiosConfig.ts
+++ front/src/apis/axiosConfig.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
+
 // import { getCookie} from './cookie';
 // import { getCookie, setCookie } from 'typescript-cookie'
 
 // 로그인된 사용자
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
 // const BASE_URL = "http://buddiaryALB-1250245218.ap-northeast-2.elb.amazonaws.com";
-const FASTAPI_BASE_URL = "http://localhost:9000";
-const NAVER_CLOVA_URL =
-  "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze";
+const FASTAPI_BASE_URL = `${process.env.REACT_APP_FASTAPI_BASE_URL}`;
+const NAVER_CLOVA_URL = `${process.env.REACT_APP_NAVER_CLOVA_URL}`;
 // const BASE_URL = "http://3.35.197.93:8080";
 // const BASE_URL = "http://172.31.144.1:8080";
 // const BASE_URL = "http://192.168.100.175:8080";
@@ -46,10 +46,11 @@ const sentimentApi = axios.create({
   },
 });
 
+console.log(process.env.KAKAO_REDIRECT);
 // 카카오톡 로그인
-const REDIRECT_URI = "http://localhost:3000/login/oauth2/code/kakao";
+const REDIRECT_URI = `${process.env.REACT_APP_KAKAO_REDIRECT}`;
 // const REDIRECT_URI = "http://ec2-3-36-102-176.ap-northeast-2.compute.amazonaws.com/login/oauth2/code/kakao";
-const KAKAO_AUTH_URL = `http://localhost:8080/oauth2/authorize/kakao?redirect_uri=${REDIRECT_URI}`;
+const KAKAO_AUTH_URL = `${process.env.REACT_APP_KAKAO_AUTH_URL}${REDIRECT_URI}`;
 // const KAKAO_AUTH_URL = `http://buddiaryALB-1250245218.ap-northeast-2.elb.amazonaws.com/oauth2/authorize/kakao?redirect_uri=${REDIRECT_URI}`;
 console.log(KAKAO_AUTH_URL);
 const kakaoApi = axios.create({
--- front/src/pages/signup/SignUpPage.tsx
+++ front/src/pages/signup/SignUpPage.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react'
-import { kakaoSignUpApi } from '../../apis/userApi';
+import React, { useEffect } from "react";
+import { kakaoSignUpApi } from "../../apis/userApi";
 // import { REDIRECT_URI } from '../../apis/axiosConfig';
 import { useNavigate } from "react-router-dom";
-import useMember from '../../hooks/memberHook';
-import Swal from 'sweetalert2';
+import useMember from "../../hooks/memberHook";
+import Swal from "sweetalert2";
 export default function SignUpPage() {
   const navigate = useNavigate();
   const { login, memberData } = useMember();
@@ -11,32 +11,29 @@ export default function SignUpPage() {
     async function fetchData() {
       const currentUrl: string = window.location.href;
       // const code = currentUrl.split('buddiaryALB-1250245218.ap-northeast-2.elb.amazonaws.com/')[1];
-      const code = currentUrl.split('localhost:3000/')[1];
-      const response = await kakaoSignUpApi(code)
-      console.log(response)
+      const code = currentUrl.split(
+        `${process.env.REACT_APP_KAKAO_PARSE_URI}`
+      )[1];
+      const response = await kakaoSignUpApi(code);
+      console.log(response);
       // 처음 가입한 사람이면 signup 으로 보내고, 아니면 메인페이지로
-      if (response.newBe === true ) {
-        login(response)
-        navigate('/signup-info', {state:response})
-
+      if (response.newBe === true) {
+        login(response);
+        navigate("/signup-info", { state: response });
       } else if (response.newBe === false) {
-          
         // 이미 가입된 사용자이면
-        login(response)
+        login(response);
         // if (response.)
         Swal.fire({
-          icon: 'success',
-          text: '로그인 성공!'
-        })
-        navigate('/')
-        
+          icon: "success",
+          text: "로그인 성공!",
+        });
+        navigate("/");
       } else {
-        console.log(response)
+        console.log(response);
       }
     }
     fetchData();
   }, []);
-  return (
-    <div>SignUpPage</div>
-  )
+  return <div>SignUpPage</div>;
 }
