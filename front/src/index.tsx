f6d0b8738b3015dc303a4868dc49eaf7661f54f7
raichu1706@gmail.com <raichu1706@gmail.com>
2023-05-17 17:18:25 +0900

    초대승인

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -20,8 +20,7 @@ import ViewDiariesPage from "./pages/view/ViewDiariesPage";
 import SignUpInfoPage from "./pages/signup/SignUpInfoPage";
 import StickerStorePage from "./pages/stickers/StickerStorePage";
 import DecoratePage from "./pages/decorate/DecoratePage";
-
-import applyInvitation from "./components/kakaoinvitation/applyInvitation";
+import ApproveInvitation from "./components/kakaoinvitation/approveInvitation";
 // const {isLoggedIn} = useMember();
 const router = createBrowserRouter([
   {
@@ -45,7 +44,7 @@ const router = createBrowserRouter([
       { path: "/signup-info", element: <SignUpInfoPage /> },
       { path: "/stickers", element: <StickerStorePage /> },
       { path: "/decorate/:id", element: <DecoratePage /> },
-      { path: "/group/apply/:id", element: <applyInvitation /> },
+      { path: "/group/approve/:id", element: <ApproveInvitation /> },
     ],
   },
 ]);
